94df28052aee948741e84dbf7af79d48e627850e
DilandLoF <115326910+Diland-sketch@users.noreply.github.com>
2024-05-15 10:03:29 -0500

    Add files via upload

---
+++ TallerSistemaAlojamientoTresCapas/src/excepciones/ExcepcionFechaAlojamiento.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author ESTUDIANTES
+ */
+public class ExcepcionFechaAlojamiento extends Exception{
+
+    public ExcepcionFechaAlojamiento(String message) {
+        super(message);
+    }
+    
+}
